d98a7ae9357367b516dec1e24628aa339939a553
srgooglo <chippotato39@gmail.com>
2020-11-17 14:12:36 +0100

    added welcome

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,23 +5,14 @@ import { FormattedMessage, FormattedNumber } from 'react-intl'
 
 @connect(({ app }) => ({ app }))
 export default class Index extends React.Component {
-    state = {
-        
-    }
-
-    openMenu(id) {
-        try {
-            history.push(`/${id}`)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
     render() {
         const session = this.props.app.session
         return (
             <div>
-                <h1><FormattedMessage id="welcome_index" values={{ user: session.fullName ?? session.username ?? "Default" }} defaultMessage={"Welcome! {user}"} /></h1>
+                <h1 style={{ fontWeight: 700, fontSize: '31px' }}>
+                    <antd.Avatar style={{ marginRight: "17px" }} size="large" shape="square" src="https://www.flaticon.com/svg/static/icons/svg/149/149071.svg" />
+                    <FormattedMessage id="welcome_index" values={{ user: session.fullName ?? session.username ?? "Default" }} defaultMessage={"Welcome! {user}"} />
+                </h1>
             </div>
         )
     }
