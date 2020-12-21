d203ae10a2ef38bcc14d360308910b64cb451a07
srgooglo <chippotato39@gmail.com>
2020-12-21 14:22:01 +0100

    update versions

--- .umirc.js
+++ .umirc.js
@@ -2,35 +2,35 @@ import { defineConfig } from 'umi';
 import path from 'path';
 
 export default defineConfig({
-    hash: true,
-    //devtool: 'eval',
-    dva: { immer: true },
-    targets: { ie: 11 },
-    ignoreMomentLocale: true,
-    nodeModulesTransform: {
-        type: 'none',
-    },
-    alias: {
-        globals: path.resolve(__dirname, './globals'),
-        theme: path.resolve(__dirname, './src/theme'),
-        locales: path.resolve(__dirname, './src/locales'),
-        core: path.resolve(__dirname, './src/core'),
-        config: path.resolve(__dirname, './config'),
-        pages: path.resolve(__dirname, './src/pages'),
-        components: path.resolve(__dirname, './src/components'),
-        models: path.resolve(__dirname, './src/models'),
-        node_modules: path.resolve(__dirname, './node_modules')
-    },
-    extraBabelPresets: ['@lingui/babel-preset-react'],
-    extraBabelPlugins: [
-        [
-          'import',
-          {
-            libraryName: 'lodash',
-            libraryDirectory: '',
-            camel2DashComponentName: false,
-          },
-          'lodash',
-        ],
-      ],
+  hash: true,
+  //devtool: 'eval',
+  dva: { immer: true },
+  targets: { ie: 11 },
+  ignoreMomentLocale: true,
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  alias: {
+    globals: path.resolve(__dirname, './globals'),
+    theme: path.resolve(__dirname, './src/theme'),
+    locales: path.resolve(__dirname, './src/locales'),
+    core: path.resolve(__dirname, './src/core'),
+    config: path.resolve(__dirname, './config'),
+    pages: path.resolve(__dirname, './src/pages'),
+    components: path.resolve(__dirname, './src/components'),
+    models: path.resolve(__dirname, './src/models'),
+    node_modules: path.resolve(__dirname, './node_modules')
+  },
+  extraBabelPresets: ['@lingui/babel-preset-react'],
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'lodash',
+    ],
+  ],
 })
