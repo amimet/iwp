0dc2ffcaaaf3d583aaac5076ad96e27756e00bf4
srgooglo <chippotato39@gmail.com>
2021-01-12 13:34:06 +0100

    udpated antd, added api method

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import * as antd from 'antd'
 
 export default class Users extends React.Component {
-    
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        window.dispatcher({
+            type: "api/request",
+            payload: {
+                method: "GET",
+                endpoint: "users"
+            },
+            callback: (err, res) => {
+                console.log(res)
+            }
+        })
+    }
+
     render() {
-        return <div>
-            
-        </div>
+        return <antd.List 
+            dataSource={this.state.list}
+            renderItem={(item) => {
+                console.log(item)
+                return <div>
+
+                </div>
+            }}
+        />         
     }
 }
