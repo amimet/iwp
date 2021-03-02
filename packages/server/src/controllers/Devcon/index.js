6530be1f54d639e29ae88bda5812a8df645f1038
srgooglo <chippotato39@gmail.com>
2021-03-02 15:44:20 +0100

    added devcon

---
+++ packages/server/src/controllers/Devcon/index.js
@@ -0,0 +1,19 @@
+import printer from '../../lib/printer' 
+
+
+export const Devcon = {
+    get: (req, res, next) => {
+        console.log(printer)
+    },
+    update: (req, res, next) => {
+
+    },
+    remove: (req, res, next) => {
+
+    },
+    set: (req, res, next) => {
+
+    }
+}
+
+export default Devcon
---
+++ packages/server/src/lib/printer/index.js
@@ -0,0 +1,3 @@
+import modules from '@nodecorejs/modules'
+
+console.log(modules)
