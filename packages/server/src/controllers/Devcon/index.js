e64da0cfc233d1a736f793547552607bab498e61
srgooglo <chippotato39@gmail.com>
2021-03-03 15:32:05 +0100

    added sendprinter

--- packages/server/src/controllers/Devcon/index.js
+++ packages/server/src/controllers/Devcon/index.js
@@ -1,9 +1,19 @@
-import printer from '../../lib/printer' 
-
+import printer from '../../lib/printer'
 
 export const Devcon = {
+    sendPrinter: (req, res, next) => {
+        const { data, type } = req.body
+        console.log(data, type)
+        printer.sendPrint({ data, type })
+            .then((res) => {
+                res.json(res)
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+    },
     get: (req, res, next) => {
-        console.log(printer)
+
     },
     update: (req, res, next) => {
 
