cb65b8a0aca3eb88ccdd7ee7354e6161292175e7
srgooglo <srgooglo@ragestudio.net>
2021-07-29 12:09:11 +0200

    refactor drawerController

--- packages/admin/src/core/uiHelpers/index.js
+++ packages/admin/src/core/uiHelpers/index.js
@@ -1,11 +1,20 @@
 import Login from 'pages/login'
 
 export function openLoginDrawer(){
-    window.controllers.drawer.open(Login, {
+    window.controllers.drawer.open("login", Login, {
+        locked: true,
         componentProps: {
             onDone: () => window.controllers.drawer.close(),
         },
+        extend: [
+            (self) => {
+                self.onDone = () => {
+                    self.close()
+                }
+            },
+        ],
         props: {
+            closable: false,
             width: "45%",
         },
     })
--- packages/admin/src/layout/components/drawer/index.jsx
+++ packages/admin/src/layout/components/drawer/index.jsx
@@ -3,127 +3,138 @@ import { Controller } from "core/libs"
 import * as antd from "antd"
 
 export class Drawer extends React.Component {
-    render() {
-        return <antd.Drawer
-            visible={this.props.render}
-        >
+	options = this.props.options ?? {}
 
-        </antd.Drawer>
-    }
+	state = {
+		locked: this.options.locked ?? false,
+		visible: this.props.children ? true : false,
+	}
+
+	unlock = () => this.setState({ locked: false })
+	lock = () => this.setState({ locked: true })
+
+	close = () => {
+		if (typeof this.options.props?.closable !== "undefined" && !this.options.props?.closable) {
+			return false
+		}
+
+		if (typeof this.options.onClose === "function") {
+			this.options.onClose(...context)
+		}
+
+		this.setState({ visible: false })
+		this.unlock()
+
+		setTimeout(() => {
+			this.props.controller.destroy(this.props.id)
+		}, 400)
+	}
+
+	componentDidMount = () => {
+		if (typeof this.props.controller === "undefined") {
+			throw new Error(`Cannot mount an drawer without an controller`)
+		}
+
+		if (Array.isArray(this.options.extends)) {
+			this.options.extends.forEach((extend) => {
+				if (typeof extend === "function") {
+					extend(this)
+				}
+			})
+		}
+	}
+
+	render() {
+		const drawerProps = {
+			...this.options.props,
+			onClose: this.onClose,
+			ref: this.props.ref,
+			visible: this.state.visible,
+		}
+		const componentProps = {
+			...this.options.componentProps,
+			onDone: this.onDone,
+			onFail: this.onFail,
+		}
+
+		return <antd.Drawer {...drawerProps}>{React.createElement(this.props.children, componentProps)}</antd.Drawer>
+	}
 }
 
 export default class DrawerController extends React.Component {
 	windowController = new Controller({ id: "drawer", locked: true })
 
 	state = {
-        address: {},
-		instances: [],
+		addresses: {},
+		refs: {},
+		drawers: [],
 	}
 
 	componentDidMount() {
-		this.windowController.add(
-			"open",
-			this.open,
-			{ lock: true },
-		)
-
-		this.windowController.add(
-			"close",
-			this.close,
-			{ lock: true },
-		)
+		this.windowController.add("open", this.open, { lock: true })
+		this.windowController.add("close", this.close, { lock: true })
 	}
 
-	lock = () => {}
+	open = (id, component, options) => {
+		const refs = this.state.refs ?? {}
+		const drawers = this.state.drawers ?? []
+		const addresses = this.state.addresses ?? {}
 
-	unlock = () => {}
-
-	open = (component, options) => {
-        const instances = this.state.instances ?? []
-
-        instances.push(new Drawer())
-        this.setState({ instances })
-
-
-        return 
-    }
-
-	close = (component) => {}
-
-	handleDrawerEvent(e, instance) {
-		let drawerInstance = this.state.instance
-
-		const event = { ...e }
-		const { eventInstance, type } = event
-
-		if (typeof eventInstance !== "string") {
-			console.warn(`eventInstance is not defined / valid, handling with default event`)
+		const instance = {
+			id,
+			ref: React.createRef(),
+			children: component,
+			options,
+			controller: this,
 		}
 
-		const isOpenDrawer = () => Boolean(drawerInstance.render ? true : false)
-		const isLocked = () => Boolean(drawerInstance.options?.lock ?? false)
+		if (typeof addresses[id] === "undefined") {
+			drawers.push(<Drawer {...instance} />)
+			addresses[id] = drawers.length - 1
+			refs[id] = instance.ref
+		} else {
+			const ref = refs[id].current
+			const isLocked = ref.state.locked
 
-		switch (eventInstance) {
-			case "lock": {
-				return this.handleUpdateDrawerInstance({ options: { lock: true } })
+			if (!isLocked) {
+				drawers[addresses[id]] = <Drawer {...instance} />
+				refs[id] = instance.ref
+			} else {
+				console.warn("Cannot update an locked drawer.")
 			}
-			case "unlock": {
-				return this.handleUpdateDrawerInstance({ options: { lock: false } })
-			}
-			case "onClose": {
-				if (isLocked()) {
-					console.warn(`Drawer is locked, render update is not allowed`)
-					return false
-				}
-				drawerInstance.render = null
-				return this.handleUpdateDrawerInstance(drawerInstance)
-			}
-			case "open": {
-				let timeout = 0
-				if (isOpenDrawer() && isLocked()) {
-					console.warn(`Drawer is locked, render update is not allowed`)
-					return false
-				}
-
-				if (drawerInstance.render) {
-					timeout = 250
-					this.handleDrawerEvent({ eventInstance: "onClose" })
-				}
-
-				setTimeout(() => {
-					if (typeof instance.options !== "undefined") {
-						drawerInstance.options = { ...drawerInstance.options, ...instance.options }
-					}
-
-					drawerInstance.render = instance?.fragment
-					this.handleUpdateDrawerInstance(drawerInstance)
-				}, timeout)
-			}
-			default:
-				break
 		}
+
+		this.setState({ refs, addresses, drawers })
 	}
 
-	renderInstances = () => {
-		if (Array.isArray(this.state.instances)) {
-			return this.state.instances.map((instance) => {
-				return (
-					<Drawer visible={instance.component} {...instance.options.props}>
-						<React.Fragment>
-							{React.createElement(instance.component, {
-								...this.props,
-								...instance.options.componentProps,
-							})}
-						</React.Fragment>
-					</Drawer>
-				)
-			})
-		}
+	destroy = (id) => {
+		let { addresses, drawers, refs } = this.state
+		const index = addresses[id]
 
-		return <div></div>
+		if (typeof drawers[index] !== "undefined") {
+			drawers = drawers.filter((value, i) => i !== index)
+		}
+		delete addresses[id]
+		delete refs[id]
+
+		this.setState({ addresses, drawers })
+	}
+
+	close = (id) => {
+		const ref = this.state.refs[id]?.current
+
+		if (typeof ref !== "undefined") {
+			if (ref.state.locked && ref.state.visible) {
+				return console.warn("This drawer is locked and cannot be closed")
+			} else {
+				return ref.close()
+			}
+		} else {
+			return console.warn("This drawer not exists")
+		}
 	}
 
 	render() {
-		return <div>{this.renderInstances()}</div>
+		return this.state.drawers
 	}
 }
