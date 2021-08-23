170b4621bf80992d835c91f33f6a4307e946ce4a
srgooglo <srgooglo@ragestudio.net>
2021-08-23 15:07:48 +0200

    added fabric controlelr

--- packages/server/src/controllers/FabricController/index.js
+++ packages/server/src/controllers/FabricController/index.js
@@ -1,16 +1,44 @@
+import { FabricObject } from '../../models'
+import { Schematized } from '../../lib'
+
 export const FabricController = {
-    get: (req, res, next) => {
+    get: async (req, res) => {
+        const obj = await FabricObject.findOne({ ...req.query })
+
+        return res.json(obj)
+    },
+    getAll: async (req, res) => {
+        const objects = await FabricObject.find({ ...req.query })
+
+        return res.json(objects)
+    },
+    create: Schematized({
+        id: { required: true, type: String },
+        title: { required: true, type: String },
+        description: String,
+    }, async (req, res) => {
+        const { id, title, description, img, props, cost, timeSpend } = req.body
+
+        let craft = { id, title, description, img, props, cost, timeSpend }
+
+        try {
+            const objects = await FabricObject.find({ id })
+
+            if (objects.length > 0) {
+                return res.status(409).send("Item already exists!")
+            }
+
+            const obj = new FabricObject(craft)
+            obj.save()
+
+            return res.json(obj)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }),
+    remove: (req, res) => {
 
     },
-    update: (req, res, next) => {
-
-    },
-    remove: (req, res, next) => {
-
-    },
-    set: (req, res, next) => {
-
-    }
 }
 
 export default FabricController
