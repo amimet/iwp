4bde8f14fedce4c696a872da693b7bf2d282270b
srgooglo <srgooglo@ragestudio.net>
2021-08-23 15:06:57 +0200

    lint

--- packages/server/src/schemas/user/index.js
+++ packages/server/src/schemas/user/index.js
@@ -4,7 +4,7 @@ export const UserSchema = Schema({
     username: { type: String, required: true },
     password: { type: String, required: true, select: false },
     fullName: String,
-    avatar: {type: String, default: "https://www.flaticon.com/svg/static/icons/svg/149/149071.svg"},
+    avatar: { type: String, default: "https://www.flaticon.com/svg/static/icons/svg/149/149071.svg" },
     email: String,
     roles: [],
     legal_id: String,
