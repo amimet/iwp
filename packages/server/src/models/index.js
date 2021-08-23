7c40f6f3b9ab2d2f7d27e712fbcd2e4ee7e67847
srgooglo <srgooglo@ragestudio.net>
2021-08-23 15:07:13 +0200

    updated fabric model

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import * as schemas from '../schemas'
 
-export const Fabric = mongoose.model('Fabric', schemas.FabricSchema, "fabric")
+export const FabricObject = mongoose.model('FabricObject', schemas.FabricObjectSchema, "fabricObjects")
 export const Workload = mongoose.model('Workload', schemas.WorkloadSchema, "workload")
 export const Role = mongoose.model('Role', schemas.RoleSchema, 'roles')
 export const Vault = mongoose.model('Vault', schemas.VaultSchema, "vault")
