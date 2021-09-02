d4183ca858212f423abf30aa7958709b8200a28a
srgooglo <srgooglo@ragestudio.net>
2021-09-02 14:29:48 +0200

    removed unused methods

--- packages/admin/src/pages/main/index.jsx
+++ packages/admin/src/pages/main/index.jsx
@@ -1,35 +1,9 @@
 import React from "react"
 
-export default class MainTest extends React.Component {
-	setCartState = (payload) => {
-		const [state, dispatch] = this.props.withGlobalState()
-		dispatch({ type: "UPDATE_CART", payload })
-	}
-
-	sumItems = () => {
-		const [state, dispatch] = this.props.withGlobalState()
-		dispatch({ type: "UPDATE_CART", payload: {
-			items: (state.cart?.items ?? 0) + 1
-		}})
-	}
-
+export default class Main extends React.Component {
 	render() {
-		const [ state, dispatch ] = this.props.withGlobalState()
 		return <div>
-			STATE:
-
-			<div>
-			{JSON.stringify(state)}
-			</div>
-
-			<div>
-				<button onClick={() => this.setCartState({ mayonese: 30 })}>
-					set cart
-				</button>
-				<button onClick={() => this.sumItems()}>
-					sum
-				</button>
-			</div>
+		
 		</div>
 	}
 }
