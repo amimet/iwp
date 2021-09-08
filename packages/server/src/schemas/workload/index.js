14076a1853e02242528971f2db130cda85534484
srgooglo <srgooglo@ragestudio.net>
2021-09-08 15:55:05 +0200

    update schema

--- packages/server/src/schemas/workload/index.js
+++ packages/server/src/schemas/workload/index.js
@@ -3,6 +3,6 @@ import { Schema } from 'mongoose'
 export const WorkloadSchema = Schema({
     created: { type: Number, required: true },
     items: { type: Object, required: true },
-    region: String,
-    expiration: Number,
+    region: { type: String, default: "global" },
+    expiration: { type: Number },
 })
