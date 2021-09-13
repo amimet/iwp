f66a4d3a3cd1d8186ff286f360e02acff62f8c6d
srgooglo <srgooglo@ragestudio.net>
2021-09-13 09:29:30 +0200

    fix render

--- packages/admin/src/index.jsx
+++ packages/admin/src/index.jsx
@@ -5,25 +5,4 @@ import App from "./App"
 import "theme/index.less"
 import "antd/dist/antd.less"
 
-function render(props) {
-  const { container } = props;
-  ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'))
-}
-
-if (!window.__POWERED_BY_QIANKUN__) {
-  render({})
-}
-
-export async function bootstrap() {
-	console.log("[react16] react app bootstraped")
-}
-
-export async function mount(props) {
-	console.log("[react16] props from main framework", props)
-	render(props)
-}
-
-export async function unmount(props) {
-	const { container } = props
-	ReactDOM.unmountComponentAtNode(container ? container.querySelector("#root") : document.querySelector("#root"))
-}
+ReactDOM.render(<App />, document.querySelector("#root"))
