3c5172a526cfbfc06678178a85c4397dc2166030
srgooglo <srgooglo@ragestudio.net>
2021-09-29 15:29:54 +0200

    update extensions exports

--- packages/admin/src/extensions/index.js
+++ packages/admin/src/extensions/index.js
@@ -1,3 +1,5 @@
 export { default as Render } from './render'
 export { default as API } from './api'
 export { default as Splash } from './splash'
+export { default as Debug } from './debug'
+export { default as connect } from './connect'
