0264caa0016511474f3459f38d85c4bfc92fcbbd
srgooglo <srgooglo@ragestudio.net>
2021-09-30 14:02:36 +0200

    update user schema

--- packages/server/src/schemas/user/index.js
+++ packages/server/src/schemas/user/index.js
@@ -7,7 +7,6 @@ export const UserSchema = Schema({
     avatar: { type: String, default: "https://www.flaticon.com/svg/static/icons/svg/149/149071.svg" },
     email: String,
     roles: [],
-    legal_id: String,
-    legal_id_type: String,
-    contact_phone: String,
+    legal_id: Object,
+    phone: Number,
 })
