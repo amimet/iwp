83e1a96a4b33a07df827a3184edeb2135c26dc8f
srgooglo <srgooglo@ragestudio.net>
2021-09-30 15:07:47 +0200

    extend evite debugger

--- packages/admin/src/debug/evite/index.jsx
+++ packages/admin/src/debug/evite/index.jsx
@@ -1,17 +1,56 @@
 import React from "react"
 import ReactJson from "react-json-view"
 
-export default () => {
+import "./index.less"
+
+export default (props = {}) => {
+	const { appBinding } = props
+
+	console.log(appBinding)
+	
+	const getClassname = (classname) => {
+		return `evite-debugger_${classname}`
+	}
+
+	const getExtensions = () => {
+		const extensions = {}
+
+		Array.from(appBinding.constructorContext.extensions).forEach((extension) => {
+			extensions[extension.key] = extension
+		})
+
+		return extensions
+	}
+
 	return (
 		<div>
-			<div>
-				<h4>namespace</h4>
-				<ReactJson name="window.__evite" collapsed="true" src={window.__evite} />
+			{!appBinding && (
+				<div className={getClassname("warning_bindingDisabled")}>
+					<strong>
+						⚠️ Warning! Cannot access to <code>evite.appBinding</code>, debugger is limited.
+					</strong>
+				</div>
+			)}
+			<div className={getClassname("content")}>
+				<div>
+					<h4>📦 Namespace</h4>
+					<div>
+						<ReactJson name="window.__evite" collapsed="true" src={window.__evite} />
+					</div>
+				</div>
+				<div>
+					<h4>📦 AppContext</h4>
+					<div>
+						<ReactJson name="app" collapsed="true" src={appBinding} />
+					</div>
+				</div>
+				<div>
+					<h4>🧰 Extensions</h4>
+					<div>
+						<ReactJson name="extensions" collapsed="true" src={getExtensions()} />
+					</div>
+				</div>
 			</div>
-            <div>
-                <h4>extensions</h4>
-                
-            </div>
 		</div>
 	)
 }
--- packages/admin/src/debug/less/index.jsx
+++ packages/admin/src/debug/less/index.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import less from "less"
 
 export default () => {
-    function getLessVars() {
-        return less.getVars()
-    }
+    console.log(less)
 
     return <div>
         <h4>Current Variables</h4>
-        {JSON.stringify(getLessVars(), null, 2)}
+     
     </div>
 }
