49a73968c7d3bcd055b48a22562f7d3c1e66a00d
srgooglo <srgooglo@ragestudio.net>
2021-10-04 15:36:16 +0200

    use `getSchemas`

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -1,12 +1,27 @@
 import mongoose from 'mongoose'
-import * as schemas from '../schemas'
+import { Schema } from 'mongoose'
 
-export const FabricObject = mongoose.model('FabricObject', schemas.FabricObjectSchema, "fabricObjects")
-export const Workload = mongoose.model('Workload', schemas.WorkloadSchema, "workload")
-export const Role = mongoose.model('Role', schemas.RoleSchema, 'roles')
-export const Vault = mongoose.model('Vault', schemas.VaultSchema, "vault")
-export const GeoRegion = mongoose.model('GeoRegion', schemas.GeoRegionSchema, "regions")
+function getSchemas() {
+    const obj = Object()
+    
+    const _schemas = require("../schemas")
+    Object.keys(_schemas).forEach(key => {
+        obj[key] = Schema(_schemas[key])
+    })
 
-export const Contract = mongoose.model('Contract', schemas.ContractSchema, "contracts")
-export const User = mongoose.model('User', schemas.UserSchema, "accounts")
-export const Session = mongoose.model('Session', schemas.SessionSchema, "sessions")
+    return obj
+}
+
+const schemas = getSchemas()
+
+export const FabricObject = mongoose.model('FabricObject', schemas.FabricObject, "fabricObjects")
+export const Workload = mongoose.model('Workload', schemas.Workload, "workload")
+export const Workshifts = mongoose.model('Workshifts', schemas.Workshift, "workshifts")
+export const Role = mongoose.model('Role', schemas.Role, 'roles')
+export const Vault = mongoose.model('Vault', schemas.VaultItem, "vault")
+export const GeoRegion = mongoose.model('GeoRegion', schemas.Region, "regions")
+
+export const Contract = mongoose.model('Contract', schemas.Contract, "contracts")
+export const User = mongoose.model('User', schemas.User, "accounts")
+export const Session = mongoose.model('Session', schemas.Session, "sessions")
+export const Workshift = mongoose.model("Workshift", schemas.Workshift, "workshifts")
