0a9c98dc534358c99f14aa035909fada58cfeeeb
srgooglo <srgooglo@ragestudio.net>
2021-10-07 14:22:00 +0200

    added `expired` to schema

--- packages/server/src/schemas/workload/index.js
+++ packages/server/src/schemas/workload/index.js
@@ -7,6 +7,7 @@ export default {
     scheduledStart: { type: String },
     scheduledFinish: { type: String },
     status: { type: String, default: 'pending' },
+    expired: { type: Boolean, default: false },
     assigned: { type: Array, default: [] },
     phase: { type: Number, default: false },
     nof: { type: Number, default: false},
