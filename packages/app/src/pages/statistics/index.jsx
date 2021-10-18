7c44e29274af5fc21a4c4b646671bfc111618870
srgooglo <srgooglo@ragestudio.net>
2021-10-18 15:12:06 +0200

    added `statistics` page

---
+++ packages/app/src/pages/statistics/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Statistics extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Statistics</h1>
+      </div>
+    )
+  }
+}
