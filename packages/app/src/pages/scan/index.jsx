f69365592c577c190cf67e718c336b453cabb988
srgooglo <srgooglo@ragestudio.net>
2021-10-18 15:11:51 +0200

    added `scan` pages

---
+++ packages/app/src/pages/scan/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as antd from 'antd'
+
+import "./index.less"
+
+export default class Scan extends React.Component {
+    render() {
+        return <div>
+            
+        </div>
+    }
+}
