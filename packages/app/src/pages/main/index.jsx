1de8ad1d05ae22ad3b789780b4eef92488c91bd5
srgooglo <srgooglo@ragestudio.net>
2021-10-22 15:19:46 +0200

    update main

--- packages/app/src/pages/main/index.jsx
+++ packages/app/src/pages/main/index.jsx
@@ -1,6 +1,86 @@
 import React from "react"
-export default class Main extends React.Component {
+import * as antd from "antd"
+import { AppSearcher } from "components"
+import * as charts from "react-chartjs-2"
+
+import "./index.less"
+
+const data = {
+	labels: ["Red", "Orange", "Blue"],
+	datasets: [
+		{
+			label: "Popularity of colours",
+			data: [55, 23, 96],
+			borderWidth: 5,
+			fill: false,
+		},
+	],
+}
+
+const chartKeys = Object.fromEntries(Object.keys(charts).map((key) => {
+	return [String(key).toLowerCase(), key]
+}))
+
+class ChartGenerator extends React.Component {
+	constructor(payload) {
+		super(payload)
+		this.payload = payload
+
+		this.type = this.payload.type
+		this.Chart = charts[this.type] ?? charts[chartKeys[this.type]]
+
+		this.state = {
+			labels: [],
+			datasets: [],
+		}
+	
+		if (!this.Chart) {
+			console.error("Chart type is not valid")
+		}
+	}
+	
 	render() {
-		return <div></div>
+		const { Chart } = this
+
+		if (React.isValidElement(Chart)) {
+			return null
+		}
+
+		return <Chart data={this.state}/>
+	}
+}
+
+export default class Main extends React.Component {
+	state = {}
+
+	componentWillUnmount = () => {
+		if (!window.headerVisible) {
+			window.toogleHeader(true)
+		}
+	}
+
+	render() {
+		if (window.headerVisible) {
+			window.toogleHeader(false)
+		}
+
+		const user = this.props.user ?? {}
+		console.log(user)
+		return (
+			<div className="dashboard">
+				<div className="top">
+					<div>
+						<h1>Welcome back, {user.fullName ?? user.username ?? "Guest"}</h1>
+					</div>
+					<div>
+						<AppSearcher />
+					</div>
+				</div>
+				<div className="content">
+					<h2>Statistics</h2>
+					<div><ChartGenerator type="line" /></div>
+				</div>
+			</div>
+		)
 	}
 }
