0f86aeab01f7615c982fadb4a130c1bf90cc4fb7
srgooglo <srgooglo@ragestudio.net>
2021-10-22 15:19:05 +0200

    update header for `appSearcher`

--- packages/app/src/layout/components/header/index.jsx
+++ packages/app/src/layout/components/header/index.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import * as antd from 'antd'
-import classnames from 'classnames'
+import React from "react"
+import * as antd from "antd"
+import { AppSearcher } from "components"
+import classnames from "classnames"
 
 import "./index.less"
 
 export default class Header extends React.Component {
-    state = {
-        loadingSearch: false,
-        visible: true
-    }
+	state = {
+		loadingSearch: false,
+		visible: true,
+	}
 
-    componentDidMount() {
-        window.toogleHeader = (to) => {
-            this.setState({ visible: to ?? !this.state.visible })
-        }
-    }
+	componentDidMount() {
+		window.toogleHeader = (to) => {
+			this.setState({ visible: to ?? !this.state.visible })
+		}
+	}
 
-    render() {
-        window.headerVisible = this.state.visible
+	render() {
+		window.headerVisible = this.state.visible
 
-        return (
-            <antd.Layout.Header className={classnames(`app_header`, { ["hidden"]: !this.state.visible })} >
-                <antd.Input.Search className="app_searchBar" placeholder="Search on app..." loading={this.state.loadingSearch} />
-            </antd.Layout.Header>
-        )
-    }
+		return (
+			<antd.Layout.Header className={classnames(`app_header`, { ["hidden"]: !this.state.visible })}>
+				<div>
+					<AppSearcher />
+				</div>
+			</antd.Layout.Header>
+		)
+	}
 }
