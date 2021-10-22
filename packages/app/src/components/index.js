7eb4c8ec7597d814fe037dc6c893ed9fb8fdab98
srgooglo <srgooglo@ragestudio.net>
2021-10-22 15:18:53 +0200

    added appSearcher

--- packages/app/src/components/index.js
+++ packages/app/src/components/index.js
@@ -6,6 +6,7 @@ export { default as LoadingSpinner } from './LoadingSpinner'
 export { default as Settings } from './settings'
 export { default as NotFound } from './notFound'
 export { default as AppLoading } from './AppLoading'
+export { default as AppSearcher } from './AppSearcher'
 export { default as RenderError } from './RenderError'
 
 export { default as ElementsList } from './ElementsList'
@@ -14,7 +15,6 @@ export { default as Roles } from './Roles'
 export { default as ActionsBar } from './ActionsBar' 
 export { default as SelectableList } from './SelectableList'
 export { default as ObjectInspector } from './ObjectInspector'
-
 export * as QRReader from "./QRReader"
 export * as Window from './RenderWindow'
 
