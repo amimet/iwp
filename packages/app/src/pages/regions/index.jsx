7e33b051e64738c9f73c11e57138faedc0e9a3b6
srgooglo <srgooglo@ragestudio.net>
2021-11-11 14:59:21 +0100

    Refactor

--- packages/app/src/pages/regions/index.jsx
+++ packages/app/src/pages/regions/index.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import * as antd from "antd"
 import GoogleMap from "components/googleMap"
-import { LoadingSpinner } from "components"
+import * as Icons from "react-icons/md"
+
+import "./index.less"
 
 const api = window.app.apiBridge
 
@@ -12,34 +14,48 @@ export default class Geo extends React.Component {
 
 	componentDidMount = async () => {
 		const data = await api.get.regions()
+		console.log(data)
+
 		this.setState({ data })
 	}
 
+	createNewRegion = async () => {
+		antd.Modal.confirm({
+			title: <div style={{ display: "flex",  }}>
+				<h2><Icons.MdLocationSearching style={{ marginRight: "8px",  }} /> New Location</h2>
+			</div>,
+			icon: false,
+			content: <div className="creator_modal">
+				<div>
+					<h4><Icons.MdOutlineTag /> Name</h4>
+					<div>
+						<antd.Input placeholder="room_100" />
+					</div>
+				</div>
+				<div>
+					<h4><Icons.MdTitle /> Title</h4>
+					<div>
+						<antd.Input placeholder="Room 100" />
+					</div>
+				</div>
+			</div>
+		})
+	}
+
 	renderRegions(items) {
-		if (!items || !Array.isArray(items)) {
-			console.warn(`Invalid items recived > ${typeof items}`)
-			return null
-		}
-
-		return items.filter((item) => {
-			if (!item.geo || !item.data) {
-				return false
-			}
-
-			return true
-		}).map((item) => {
+		return items.map((item) => {
 			return (
-				<div key={item.id} className="app_regions_card">
+				<div key={item._id} className="region_card">
 					<div style={{ float: "left" }}>
-						<antd.Tag>#{item.id}</antd.Tag>
-						<h1>{item.data.title ?? item.data.name}</h1>
+						<antd.Tag>#{item.name}</antd.Tag>
+						<h1>{item.title ?? item.name}</h1>
 					</div>
 					<div style={{ float: "right" }}>
 						{GoogleMap({
 							zoom: 15,
-							lat: item.geo.lat,
-							lng: item.geo.lng,
-							markerText: item.data.name,
+							lat: item.cords.lat,
+							lng: item.cords.lng,
+							markerText: item.title ?? item.name,
 						})}
 					</div>
 				</div>
@@ -48,8 +64,21 @@ export default class Geo extends React.Component {
 	}
 
 	render() {
-		if (!this.state.data) return <LoadingSpinner />
+		if (!this.state.data) return <antd.Skeleton active />
 
-		return <div className="app_regions_wrapper">{this.renderRegions(this.state.data)}</div>
+		if (this.state.data.length === 0) {
+			return <antd.Result
+				icon={<Icons.MdWrongLocation style={{ width: 100, height: 100 }} />}
+				title="No regions available"
+				extra={<antd.Button type="primary" onClick={this.createNewRegion} >Create new</antd.Button>}
+			/>
+		}
+
+		return <div className="regions_list">
+			{this.renderRegions(this.state.data)}
+			<div className="create_new" onClick={this.createNewRegion}>
+
+			</div>
+		</div>
 	}
 }
