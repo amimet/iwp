75aaff5f1f2c41d12165e1ddeab5c18756879000
srgooglo <srgooglo@ragestudio.net>
2021-11-15 14:15:53 +0100

    split with class

--- packages/app/src/pages/regions/index.jsx
+++ packages/app/src/pages/regions/index.jsx
@@ -8,6 +8,90 @@ import "./index.less"
 
 const api = window.app.apiBridge
 
+class NewRegionForm extends React.Component {
+	handleFinishNewRegion = async (values, ctx) => {
+		const { name, address } = values
+
+		ctx.toogleValidation(true)
+		ctx.clearErrors()
+
+		api.put.region({ name, address })
+			.then((err, data) => {
+				console.log(err, data)
+
+				if (response.status !== 200) {
+					ctx.shake("all")
+					ctx.error("result", response.data.error)
+
+					return false
+				}
+
+				this.props.handleDone()
+				this.props.close()
+			})
+
+
+	}
+
+	render() {
+		return <div>
+			<div style={{ display: "flex", }}>
+				<h2><Icons.MdLocationSearching style={{ marginRight: "8px", }} /> New Location</h2>
+			</div>
+			<FormGenerator
+				name="new_region_form"
+				onFinish={this.handleFinishNewRegion}
+				items={[
+					{
+						id: "name",
+						element: {
+							component: "Input",
+							icon: <Icons.MdOutlineTag />,
+							placeholder: "New region",
+							props: null
+						},
+						item: {
+							hasFeedback: true,
+							rules: [
+								{
+									required: true,
+									message: 'Please input an name!',
+								},
+							],
+							props: null
+						}
+					},
+					{
+						id: "address",
+						element: {
+							component: "Input",
+							icon: <Icons.MdMyLocation />,
+							placeholder: "Av. de Zaragoza, 70, Pamplona",
+							props: null
+						},
+						item: {
+							hasFeedback: true,
+							props: null
+						}
+					},
+					{
+						id: "submit",
+						withValidation: true,
+						element: {
+							component: "Button",
+							props: {
+								children: "Create",
+								type: "primary",
+								htmlType: "submit"
+							}
+						}
+					},
+				]}
+			/>
+		</div>
+	}
+}
+
 export default class Geo extends React.Component {
 	state = {
 		data: null,
@@ -21,67 +105,7 @@ export default class Geo extends React.Component {
 	}
 
 	createNewRegion = async () => {
-		window.app.DrawerController.open("new_region_form",
-			() => <div>
-				<div style={{ display: "flex", }}>
-					<h2><Icons.MdLocationSearching style={{ marginRight: "8px", }} /> New Location</h2>
-				</div>
-				<FormGenerator
-					name="new_region_form"
-					onFinish={(values, ref) => {
-						console.log(values, ctx)
-					}}
-					items={[
-						{
-							id: "name",
-							element: {
-								component: "Input",
-								icon: <Icons.MdOutlineTag />,
-								placeholder: "New region",
-								props: null
-							},
-							item: {
-								hasFeedback: true,
-								rules: [
-									{
-										required: true,
-										message: 'Please input an name!',
-									},
-								],
-								props: null
-							}
-						},
-						{
-							id: "address",
-							element: {
-								component: "Input",
-								icon: <Icons.MdMyLocation />,
-								placeholder: "Av. de Zaragoza, 70, Pamplona",
-								props: null
-							},
-							item: {
-								hasFeedback: true,
-								props: null
-							}
-						},
-						{
-							id: "submit",
-							withValidation: true,
-							element: {
-								component: "Button",
-								props: {
-									children: "Create",
-									type: "primary",
-									htmlType: "submit"
-								}
-							}
-						},
-					]}
-				/>
-			</div>
-		)
-
-		return false
+		window.app.DrawerController.open("new_region_form", NewRegionForm)
 	}
 
 	renderRegions(items) {
@@ -89,15 +113,14 @@ export default class Geo extends React.Component {
 			return (
 				<div key={item._id} className="region_card">
 					<div style={{ float: "left" }}>
-						<antd.Tag>#{item.name}</antd.Tag>
-						<h1>{item.title ?? item.name}</h1>
+						<h1>{item.name}</h1>
 					</div>
 					<div style={{ float: "right" }}>
 						{GoogleMap({
 							zoom: 15,
-							lat: item.cords.lat,
-							lng: item.cords.lng,
-							markerText: item.title ?? item.name,
+							lat: 0,
+							lng: 0,
+							markerText: item.name,
 						})}
 					</div>
 				</div>
