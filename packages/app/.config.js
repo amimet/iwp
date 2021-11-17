be6eae988ae85770e9950c521ed0920c91ec3444
srgooglo <srgooglo@ragestudio.net>
2021-11-17 14:31:58 +0100

    remove unused

--- packages/app/.config.js
+++ packages/app/.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const fs = require("fs")
-const lessToJS = require("less-vars-to-js")
 
 const aliases = {
     schemas: path.resolve(__dirname, './schemas'),
@@ -13,31 +11,22 @@ const aliases = {
     models: path.join(__dirname, 'src/models'),
 }
 
-// require("vite-plugin-require").default(),
-
 module.exports = (config) => {
     if (typeof config.windowContext.process === 'undefined') {
         config.windowContext.process = Object()
     }
-    
+
     config.windowContext.process = config.windowContext.__evite
     config.windowContext.process["versions"] = process.versions
     config.resolve.alias = {
         ...config.resolve.alias,
         ...aliases,
     }
-    
-    // config.define = {
-    //     global: Object()
-    // }
 
     config.css = {
         preprocessorOptions: {
             less: {
                 javascriptEnabled: true,
-                // modifyVars: lessToJS(
-                //     fs.readFileSync(path.resolve(__dirname, "./config/variables.less"), "utf8")
-                // ),
             }
         }
     }
