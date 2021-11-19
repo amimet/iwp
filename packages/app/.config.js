9a4c95311a7f4e04abc9ad1731d01d97542e32b9
srgooglo <srgooglo@ragestudio.net>
2021-11-19 15:02:16 +0100

    change aliases

--- packages/app/.config.js
+++ packages/app/.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
 const aliases = {
+    "@": path.resolve(__dirname, 'src'),
     schemas: path.resolve(__dirname, './schemas'),
     extensions: path.resolve(__dirname, './src/extensions'),
     theme: path.join(__dirname, 'src/theme'),
     locales: path.join(__dirname, 'src/locales'),
     core: path.join(__dirname, 'src/core'),
-    pages: path.join(__dirname, 'src/pages'),
+    "@pages": path.join(__dirname, 'src/pages'),
     components: path.join(__dirname, 'src/components'),
     models: path.join(__dirname, 'src/models'),
 }
@@ -19,6 +20,10 @@ module.exports = (config) => {
     config.windowContext.process = config.windowContext.__evite
     config.windowContext.process["versions"] = process.versions
     config.resolve.alias = {
+        ...config.resolve.alias,
+        ...aliases,
+    }
+    config.aliases = {
         ...config.resolve.alias,
         ...aliases,
     }
