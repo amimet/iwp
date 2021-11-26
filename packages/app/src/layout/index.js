3b66cc3147e51dd8b337923d3bc090da8e0161a3
srgooglo <srgooglo@ragestudio.net>
2021-11-26 09:05:09 +0100

    remove unused component

--- packages/app/src/layout/index.js
+++ packages/app/src/layout/index.js
@@ -1,12 +1,10 @@
 import Sidebar from './sidebar'
 import Header from './header'
-import Breadcrumb from './breadcrumb'
 import Drawer from './drawer'
 import Sidedrawer from './sidedrawer'
 
 export {
     Drawer,
-    Breadcrumb,
     Sidebar,
     Header,
     Sidedrawer,
