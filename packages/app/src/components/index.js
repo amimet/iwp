89fa7596c3a686d472470fbd2f7de6a7b1539351
srgooglo <srgooglo@ragestudio.net>
2021-11-26 12:36:37 +0100

    added `modifierTag`

--- packages/app/src/components/index.js
+++ packages/app/src/components/index.js
@@ -1,25 +1,26 @@
-import FormGenerator from './formGenerator'
-import ButtonMenu from './buttonMenu'
-import * as AboutApp from './AboutApp'
+import FormGenerator from "./formGenerator"
+import ButtonMenu from "./buttonMenu"
+import * as AboutApp from "./AboutApp"
 
-export { default as LoadingSpinner } from './LoadingSpinner'
-export { default as Settings } from './settings'
-export { default as NotFound } from './notFound'
-export { default as AppLoading } from './AppLoading'
-export { default as AppSearcher } from './AppSearcher'
-export { default as RenderError } from './RenderError'
+export { default as LoadingSpinner } from "./LoadingSpinner"
+export { default as Settings } from "./settings"
+export { default as NotFound } from "./notFound"
+export { default as AppLoading } from "./AppLoading"
+export { default as AppSearcher } from "./AppSearcher"
+export { default as RenderError } from "./RenderError"
 
-export { default as ElementsList } from './ElementsList'
-export { default as Sessions } from './Sessions'
-export { default as Roles } from './Roles'
-export { default as ActionsBar } from './ActionsBar' 
-export { default as SelectableList } from './SelectableList'
-export { default as ObjectInspector } from './ObjectInspector'
-export { default as FabricCreator } from './FabricCreator'
-export { default as ServerStatus } from './ServerStatus'
+export { default as ElementsList } from "./ElementsList"
+export { default as Sessions } from "./Sessions"
+export { default as Roles } from "./Roles"
+export { default as ActionsBar } from "./ActionsBar" 
+export { default as SelectableList } from "./SelectableList"
+export { default as ObjectInspector } from "./ObjectInspector"
+export { default as FabricCreator } from "./FabricCreator"
+export { default as ServerStatus } from "./ServerStatus"
+export { default as ModifierTag } from "./modifierTag"
 
 export * as QRReader from "./QRReader"
-export * as Window from './RenderWindow'
+export * as Window from "./RenderWindow"
 
 export {
     AboutApp,
