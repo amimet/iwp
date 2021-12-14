ff6deec152696bc25ce700afb50b6eff782227f6
srgooglo <srgooglo@ragestudio.net>
2021-12-14 15:36:30 +0100

    implement disable items & search

--- packages/app/src/components/UserSelector/index.jsx
+++ packages/app/src/components/UserSelector/index.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 import * as antd from "antd"
-
+import { Icons } from "components/Icons"
 import { SelectableList } from "components"
+import { debounce } from 'lodash'
+import fuse from "fuse.js"
 
 import "./index.less"
 
@@ -9,6 +11,7 @@ export default class UsersSelector extends React.Component {
     state = {
         loading: true,
         data: [],
+        searchValue: null,
     }
 
     api = window.app.request
@@ -23,7 +26,7 @@ export default class UsersSelector extends React.Component {
     }
 
     fetchUsers = async () => {
-        const data = await this.api.get.users(undefined, this.props.filter).catch((err) => {
+        const data = await this.api.get.users(undefined, { select: this.props.select }).catch((err) => {
             console.error(err)
             antd.message.error("Error fetching operators")
         })
@@ -31,8 +34,16 @@ export default class UsersSelector extends React.Component {
         this.setState({ data: data, loading: false })
     }
 
+    isExcludedId = (id) => {
+        if (this.props.excludedIds) {
+            return this.props.excludedIds.includes(id)
+        }
+
+        return false
+    }
+
     renderItem = (item) => {
-        return <div className="user_item">
+        return <div disabled={this.isExcludedId(item._id)} className="users_selector item">
             <div>
                 <antd.Avatar shape="square" src={item.avatar} />
             </div>
@@ -42,23 +53,68 @@ export default class UsersSelector extends React.Component {
         </div>
     }
 
+    search = (value) => {
+        if (typeof value !== "string") {
+            if (typeof value.target?.value === "string") {
+                value = value.target.value
+            }
+        }
+
+        if (value === "") {
+            return this.setState({ searchValue: null })
+        }
+
+        const searcher = new fuse(this.state.data, {
+            includeScore: true,
+            keys: ["username", "fullName"],
+        })
+
+        const result = searcher.search(value)
+
+        this.setState({
+            searchValue: result.map((entry) => {
+                return entry.item
+            }),
+        })
+    }
+
+    debouncedSearch = debounce((value) => this.search(value), 500)
+
+    onSearch = (event) => {
+        if (event === "" && this.state.searchValue) {
+            return this.setState({ searchValue: null })
+        }
+
+        this.debouncedSearch(event.target.value)
+    }
+
     render() {
         if (this.state.loading) {
             return <antd.Skeleton active />
         }
 
         return <div className="users_selector">
-                <SelectableList
-                    ignoreMobileActions
-                    items={this.state.data}
-                    renderItem={this.renderItem}
-                    actions={[
-                        <div call="onDone" key="done">
-                            Done
-                        </div>
-                    ]}
-                    onDone={(keys) => this.props.handleDone(keys)}
-                />
+            <div className="users_selector header">
+                <div>
+                    <antd.Input.Search
+                        placeholder="Search"
+                        allowClear
+                        onSearch={this.onSearch}
+                        onChange={this.onSearch}
+                    />
+                </div>
+            </div>
+            <SelectableList
+                ignoreMobileActions
+                items={this.state.searchValue ?? this.state.data}
+                renderItem={this.renderItem}
+                actions={[
+                    <div call="onDone" key="done">
+                        Done
+                    </div>
+                ]}
+                onDone={(keys) => this.props.handleDone(keys)}
+            />
         </div>
     }
 }
