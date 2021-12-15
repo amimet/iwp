5a8deb2306c0ece71ca79d8ba5b6d801bc2dc72c
srgooglo <srgooglo@ragestudio.net>
2021-12-15 16:16:07 +0100

    rewrite using class

--- packages/app/src/components/OperatorsAssignments/index.jsx
+++ packages/app/src/components/OperatorsAssignments/index.jsx
@@ -1,75 +1,81 @@
 import React from "react"
 import * as antd from "antd"
 import { Icons } from "components/Icons"
-import { UserSelector } from "components"
 
 import "./index.less"
 
-export default (props) => {
-    const [operatorsData, setOperatorsData] = React.useState([])
-    const api = window.app.request
+export default class OperatorsAssignments extends React.Component {
+    state = {
+        loading: true,
+        data: null,
+    }
 
-    const onClickAddAssign = () => {
-        window.app.DrawerController.open("OperatorAssignment", UserSelector, {
-            onDone: async (ctx, data) => {
-                if (typeof props.onAssignOperators === "function") {
-                    await props.onAssignOperators(ctx, data)
-                } else {
-                    ctx.close()
-                }
-            },
-            componentProps: {
-                select: { roles: ["operator"] },
-                excludedIds: props.assigned,
+    api = window.app.request
+
+    componentDidMount = async () => {
+        await this.fetchOperatorsData()
+    }
+
+    onClickAddAssign = async () => {
+        if (typeof this.props.onAssignOperator === "function") {
+            const result = await this.props.onAssignOperator()
+
+            if (result) {
+                this.fetchOperatorsData()
             }
-        })
-    }
-
-    const onClickRemoveOperator = (_id) => {
-        if (typeof props.onRemoveOperator === "function") {
-            props.onRemoveOperator(_id)
         }
     }
 
-    const fetchOperatorsData = async () => {
-        if (!Array.isArray(props.assigned) || props.assigned.length <= 0) {
-            return setOperatorsData([])
+    onClickRemoveOperator = async (_id) => {
+        if (typeof this.props.onRemoveOperator === "function") {
+            const result = await this.props.onRemoveOperator(_id)
+
+            if (result) {
+                this.fetchOperatorsData()
+            }
+        }
+    }
+
+    fetchOperatorsData = async () => {
+        if (!Array.isArray(this.props.assigned) || this.props.assigned.length <= 0) {
+            return this.setState({ data: [], loading: false })
         }
 
-        const data = await api.get.users(null, { _id: props.assigned }).catch((err) => {
+        const data = await this.api.get.users(null, { _id: this.props.assigned }).catch((err) => {
             console.error(err)
             antd.message.error("Error fetching operators")
             return []
         })
 
-        setOperatorsData(data)
+        this.setState({ data, loading: false })
     }
 
-    React.useEffect(() => {
-        fetchOperatorsData()
-    })
-
-    return <div className="operators_assignments">
-        <div>
-            <antd.List
-                dataSource={operatorsData}
-                renderItem={(item) => {
-                    return <antd.List.Item className="operators_assignments item" key={item._id}>
-                        <antd.List.Item.Meta
-                            avatar={<antd.Avatar src={item.avatar} />}
-                            title={item.fullName ?? item.username}
-                        />
-                        <div className="operators_assignments item actions">
-                            <div>
-                                <antd.Button onClick={() => { onClickRemoveOperator(item._id) }}>Remove</antd.Button>
+    render() {
+        if (this.state.loading) {
+            return <antd.Skeleton active />
+        }
+        return <div className="operators_assignments">
+            <div>
+                <antd.List
+                    dataSource={this.state.data}
+                    renderItem={(item) => {
+                        return <antd.List.Item className="operators_assignments item" key={item._id}>
+                            <antd.List.Item.Meta
+                                avatar={<antd.Avatar src={item.avatar} />}
+                                title={item.fullName ?? item.username}
+                            />
+                            <div className="operators_assignments item actions">
+                                <div>
+                                    <antd.Button onClick={() => { this.onClickRemoveOperator(item._id) }}>Remove</antd.Button>
+                                </div>
                             </div>
-                        </div>
-                    </antd.List.Item>
-                }}
-            />
+                        </antd.List.Item>
+                    }}
+                />
+            </div>
+            <div className="operators_assignments actions">
+                <antd.Button type="primary" onClick={this.onClickAddAssign}>Add</antd.Button>
+            </div>
         </div>
-        <div className="operators_assignments actions">
-            <antd.Button type="primary" onClick={onClickAddAssign}>Add</antd.Button>
-        </div>
-    </div>
+    }
 }
