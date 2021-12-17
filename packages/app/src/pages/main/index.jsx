321d7ac630a0c0d04824eb512a72fd360a9a526e
srgooglo <srgooglo@ragestudio.net>
2021-12-17 12:11:26 +0100

    improve main for mobile & added "Assigned for you tab"

--- packages/app/src/pages/main/index.jsx
+++ packages/app/src/pages/main/index.jsx
@@ -1,35 +1,47 @@
 import React from "react"
 import * as antd from "antd"
 import { Icons } from "components/Icons"
-import { AppSearcher, ServerStatus } from "components"
+import { AppSearcher, ServerStatus, Clock, AssignedWorkload } from "components"
 
 import "./index.less"
 
-export const Clock = () => {
-	const [time, setTime] = React.useState(new Date())
-
-	React.useEffect(() => {
-		const interval = setInterval(() => {
-			setTime(new Date())
-		}, 1000)
-
-		return () => clearInterval(interval)
-	}, [])
-
-	return <div className="clock">{time.toLocaleTimeString()}</div>
-}
-
+// TODO: Customizable main menu
 export default class Main extends React.Component {
+	state = {
+		assignedWorkloads: null,
+	}
+
+	api = window.app.request
+
 	componentDidMount() {
-		if (window.app?.HeaderController?.isVisible()) {
+		this.fetchAssignedWorkloads()
+		if (!window.isMobile && window.app?.HeaderController?.isVisible()) {
 			window.app.HeaderController.toogleVisible(false)
 		}
 	}
 
 	componentWillUnmount() {
-		if (!window.app?.HeaderController?.isVisible()) {
+		if (!window.isMobile && !window.app?.HeaderController?.isVisible()) {
 			window.app.HeaderController.toogleVisible(true)
 		}
+	}
+
+	fetchAssignedWorkloads = async () => {
+		const result = await this.api.get.assignedWorkloads().catch((err) => {
+			console.error(err)
+			antd.message.error("Failed to fetch assigned workloads")
+			return false
+		})
+
+		if (result) {
+			this.setState({
+				assignedWorkloads: result,
+			})
+		}
+	}
+
+	onClickAssignedWorkload = (_id) => {
+		window.app.openWorkloadDetails(_id)
 	}
 
 	render() {
@@ -40,7 +52,7 @@ export default class Main extends React.Component {
 				<div className="top">
 					<div className="header_title">
 						<div>
-							<antd.Avatar shape="square" src={user.avatar} size={120} />
+							<antd.Avatar shape="square" src={user.avatar} size={window.isMobile ? undefined : 120} />
 						</div>
 						<div>
 							<div>
@@ -49,16 +61,16 @@ export default class Main extends React.Component {
 							<div>
 								<h1>Welcome back, {user.fullName ?? user.username ?? "Guest"}</h1>
 							</div>
-							<div>
+							{!window.isMobile && <div>
 								<ServerStatus />
-							</div>
+							</div>}
 						</div>
 					</div>
-					<div>
+					{!window.isMobile && <div>
 						<AppSearcher />
-					</div>
+					</div>}
 				</div>
-				<div className="content">
+				{!window.isMobile && <div className="content">
 					<h2><Icons.Sliders /> Quick actions</h2>
 					<div className="quick_actions">
 						<div>
@@ -66,6 +78,14 @@ export default class Main extends React.Component {
 								Create
 							</antd.Button>
 						</div>
+					</div>
+				</div>}
+				<div className="assigned">
+					<h2><Icons.MdPendingActions /> Assigned for you</h2>
+					<div>
+						{this.state.assignedWorkloads ? this.state.assignedWorkloads.map((workload) => {
+							return <AssignedWorkload workload={workload} onClick={this.onClickAssignedWorkload} />
+						}) : <antd.Skeleton active />}
 					</div>
 				</div>
 			</div>
