9d8a8642f7fd05618db46d887ed58771ddaf0ebf
srgooglo <srgooglo@ragestudio.net>
2021-12-20 14:27:00 +0100

    support fetch from API

--- packages/app/src/components/Fabric/inspector/index.jsx
+++ packages/app/src/components/Fabric/inspector/index.jsx
@@ -6,15 +6,41 @@ import FORMULAS from "schemas/fabricFormulas"
 import "./index.less"
 
 export default class Inspector extends React.Component {
+    state = {
+        data: null,
+    }
+
+    api = window.app.request
+
     componentDidMount = async () => {
-        console.log(this.props.item)
+        if (this.props.item) {
+            return await this.setState({ data: this.props.item })
+        }
+        if (this.props.id) {
+            return await this.fetchData(this.props.id)
+        }
+    }
+
+    fetchData = async (_id) => {
+        const data = await this.api.get.fabricById(undefined, {
+            _id
+        }).catch((err) => {
+            console.error(err)
+            antd.message.error(`Failed to load fabric item: ${err}`)
+            return false
+        })
+
+        if (data) {
+            console.log(data)
+            return this.setState({ data })
+        }
     }
 
     renderProperties = (item) => {
         if (!item.properties) {
             return null
         }
-        
+
         return Object.keys(item.properties).map(key => {
             const PropertyRender = () => {
                 const property = item.properties[key]
@@ -27,7 +53,7 @@ export default class Inspector extends React.Component {
 
                 return property
             }
-            
+
             return (
                 <div className="fabric_inspector properties property" key={key}>
                     <div className="fabric_inspector properties property name">{String(key).toTitleCase()}</div>
@@ -38,45 +64,44 @@ export default class Inspector extends React.Component {
     }
 
     render() {
-        if (!this.props.item) {
-            console.error("No item provided")
-            return null
+        if (!this.state.data) {
+            return <antd.Skeleton active />
         }
 
-        const formula = FORMULAS[this.props.item.type]
+        const formula = FORMULAS[this.state.data.type]
 
         return <div className="fabric_inspector">
             <div className="fabric_inspector header">
-                <h3>#{this.props.item.uuid ?? "deserialized"}</h3>
+                <h3>#{this.state.data.uuid ?? this.state.data._id ?? "deserialized"}</h3>
                 <div className="fabric_inspector header title">
                     <div>
-                        <h1>{Icons[formula.icon] && createIconRender(formula.icon)}{this.props.item.name}</h1>
+                        <h1>{Icons[formula.icon] && createIconRender(formula.icon)}{this.state.data.name}</h1>
                     </div>
-                    <div className="fabric_inspector quantity">x{this.props.item.quantity}</div>
+                    {this.state.data.quantity && <div className="fabric_inspector quantity">x{this.state.data.quantity}</div>}
                 </div>
-                <h3>{this.props.item.properties?.description}</h3>
+                <h3>{this.state.data.properties?.description}</h3>
             </div>
 
             <div className="fabric_inspector properties">
-                {this.renderProperties(this.props.item)}
+                {this.renderProperties(this.state.data)}
             </div>
 
             <div className="fabric_inspector actions">
-                <div>
+                {this.props.runnable && <div>
                     <antd.Button type="primary" icon={<Icons.MdOutlinePendingActions />}>
                         Run
                     </antd.Button>
-                </div>
+                </div>}
                 <div>
                     <antd.Button icon={<Icons.Edit />}>
                         Edit
                     </antd.Button>
                 </div>
-                <div>
+                {this.props.resolvable && <div>
                     <antd.Button icon={<Icons.CheckCircle />}>
                         Mark as done
                     </antd.Button>
-                </div>
+                </div>}
             </div>
         </div>
     }
