7d67f1a8f0b5f8ac8a1191efcf5d27d418b894a8
srgooglo <srgooglo@ragestudio.net>
2021-12-20 14:26:20 +0100

    fix schematized method

--- packages/server/src/lib/schematized/index.js
+++ packages/server/src/lib/schematized/index.js
@@ -1,13 +1,21 @@
 export default (schema = {}, fn) => {
     return async (req, res, next) => {
+        if (typeof req.body === "undefined") {
+            req.body = {}
+        }
+        if (typeof req.query === "undefined") {
+            req.query = {}
+        }
+
         if (schema.required) {
             if (Array.isArray(schema.required)) {
                 const missingKeys = []
                 const requiredKeys = Array.isArray(schema.required) ? schema.required : []
 
                 for await (let key of requiredKeys) {
-                    if (typeof req.body[key] === "undefined") {
-                        missingKeys.push(key)
+                    if (typeof req.body[key] === "undefined" && typeof req.query[key] === "undefined") {
+                        req.selection[key] = req.body[key]
+                        continue
                     }
                 }
 
@@ -21,23 +29,16 @@ export default (schema = {}, fn) => {
 
         if (schema.select) {
             if (Array.isArray(schema.select)) {
-                // assign objects along request body and query. WARNING: req.body or req.query can be undefined depending on the request method
                 if (typeof req.selection !== "object") {
                     req.selection = {}
                 }
 
-                if (typeof req.body === "undefined") {
-                    req.body = {}
-                }
-                if (typeof req.query === "undefined") {
-                    req.query = {}
-                }
-
+                // assign objects along request body and query.
                 for await (let key of schema.select) {
                     if (req.body && typeof req.body[key] !== "undefined") {
                         req.selection[key] = req.body[key]
                         continue
-                    } 
+                    }
 
                     if (req.query && typeof req.query[key] !== "undefined") {
                         req.selection[key] = req.query[key]
