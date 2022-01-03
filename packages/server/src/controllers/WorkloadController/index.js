624c89326bdeb0704e9fa104d129f318604a773d
srgooglo <srgooglo@ragestudio.net>
2022-01-03 14:34:43 +0100

    update methods

--- packages/server/src/controllers/WorkloadController/index.js
+++ packages/server/src/controllers/WorkloadController/index.js
@@ -8,7 +8,7 @@ const format = "DD-MM-YYYY hh:mm"
 export default {
     action: Schematized({
         required: ["_id", "event"],
-        select: ["_id"], 
+        select: ["_id"],
     }, async (req, res) => {
         const workload = await Workload.findById(req.selection._id)
 
@@ -18,12 +18,11 @@ export default {
             })
         }
 
-        const { user_id, action } = req.selection.event
-
         switch (action) {
             case "commit": {
-                const { orderId } = req.selection.event
-                const order = await workload.orders.findById(orderId)
+                const { orderUUID } = req.selection.event
+                const order = await workload.orders.find(order => order.uuid === orderUUID)
+                console.log(order)
             }
             default: {
                 return res.status(400).json({
@@ -31,21 +30,6 @@ export default {
                 })
             }
         }
-       
-    }),
-    getAll: Schematized({
-        select: ["region", "_id", "name"],
-    }, async (req, res) => {
-        let workloads = []
-
-        if (req.selection.region === "all") {
-            delete req.selection.region
-            workloads = await Workload.find(req.selection)
-        } else {
-            workloads = await Workload.find(req.selection)
-        }
-
-        return res.json(workloads)
     }),
     appendOperators: Schematized({
         required: ["_id", "operators"],
@@ -100,59 +84,15 @@ export default {
 
         return res.json(workload)
     }),
-    getWorkloadAssignedToUserID: Schematized({
-        select: ["_id"],
-    }, async (req, res) => {
-        let workloads = await Workload.find()
-        
-        workloads = workloads.filter(workload => workload.assigned.includes(req.selection._id ?? req.user._id))
-
-        return res.json(workloads)
-    }),
-    get: Schematized({
-        select: ["region", "_id", "name"],
-    }, async (req, res, next) => {
-        let workload = await Workload.findOne(req.selection)
-
-        // parse expiration status
-        if (typeof workload.expired !== "undefined") {
-            // check if is expired
-            const endDate = moment(workload.scheduledFinish, format)
-            const now = moment().format(format)
-
-            if (endDate.isBefore(moment(now, format))) {
-                workload.expired = true
-            }
-        }
-
-        return res.json(workload)
-    }),
-    regeneratesUUID: async (req, res) => {
-        let workloads = await Workload.find()
-
-        workloads.forEach(async (workload) => {
-            if (workload.orders.length > 0) {
-
-                workload.orders.forEach((item) => {
-                    if (typeof item.uuid === "undefined") {
-                        item.uuid = nanoid()  
-                    }
-                })
-
-                await Workload.findByIdAndUpdate(workload._id, workload)
-            }
-        })
-
-        return res.json(workloads)
-    },
     set: Schematized({
         required: ["orders", "region", "name"],
     }, async (req, res) => {
-        const { orders, region, name, scheduledStart, scheduledFinish, workshift } = req.body
+        const { orders, region, name, assigned, scheduledStart, scheduledFinish, workshift } = req.body
 
         const obj = {
             created: new Date().getTime(),
             orders,
+            assigned,
             name,
             scheduledStart,
             scheduledFinish,
@@ -192,4 +132,63 @@ export default {
 
         return res.json({ deleted })
     }),
+    get: Schematized({
+        select: ["region", "_id", "name"],
+    }, async (req, res, next) => {
+        let workload = await Workload.findOne(req.selection)
+
+        // parse expiration status
+        if (typeof workload.expired !== "undefined") {
+            // check if is expired
+            const endDate = moment(workload.scheduledFinish, format)
+            const now = moment().format(format)
+
+            if (endDate.isBefore(moment(now, format))) {
+                workload.expired = true
+            }
+        }
+
+        return res.json(workload)
+    }),
+    getAll: Schematized({
+        select: ["region", "_id", "name"],
+    }, async (req, res) => {
+        let workloads = []
+
+        if (req.selection.region === "all") {
+            delete req.selection.region
+            workloads = await Workload.find(req.selection)
+        } else {
+            workloads = await Workload.find(req.selection)
+        }
+
+        return res.json(workloads)
+    }),
+    getWorkloadAssignedToUserID: Schematized({
+        select: ["_id"],
+    }, async (req, res) => {
+        let workloads = await Workload.find()
+
+        workloads = workloads.filter(workload => workload.assigned.includes(req.selection._id ?? req.user._id))
+
+        return res.json(workloads)
+    }),
+    regeneratesUUID: async (req, res) => {
+        let workloads = await Workload.find()
+
+        workloads.forEach(async (workload) => {
+            if (workload.orders.length > 0) {
+
+                workload.orders.forEach((item) => {
+                    if (typeof item.uuid === "undefined") {
+                        item.uuid = nanoid()
+                    }
+                })
+
+                await Workload.findByIdAndUpdate(workload._id, workload)
+            }
+        })
+
+        return res.json(workloads)
+    },
 }
