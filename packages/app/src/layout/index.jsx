12a26f29fe02e5bc730d1b04b56090cb6a0ba96f
srgooglo <srgooglo@ragestudio.net>
2022-01-11 13:08:11 +0100

    fix for desktop mode

--- packages/app/src/layout/index.jsx
+++ packages/app/src/layout/index.jsx
@@ -1,28 +1,13 @@
 import React from "react"
 import classnames from "classnames"
 import * as antd from 'antd'
-import { enquireScreen, unenquireScreen } from 'enquire-js'
 
 import Sidebar from './sidebar'
 import Header from './header'
 import Drawer from './drawer'
 import Sidedrawer from './sidedrawer'
-import BottomBar from "./bottombar"
 
 const LayoutRenders = {
-	mobile: (props) => {
-		return <antd.Layout className={classnames("app_layout", ["mobile"])} style={{ height: "100%" }}>
-			<antd.Layout className="content_layout">
-				<antd.Layout.Content className="layout_page">
-					<div className={classnames("fade-transverse-active", { "fade-transverse-leave": props.isOnTransition })}>
-						{props.children}
-					</div>
-				</antd.Layout.Content>
-			</antd.Layout>
-			<BottomBar user={props.user} />
-			<Drawer />
-		</antd.Layout>
-	},
 	default: (props) => {
 		return <antd.Layout className="app_layout" style={{ height: "100%" }}>
 			<Drawer />
@@ -43,7 +28,6 @@ const LayoutRenders = {
 export default class Layout extends React.Component {
 	state = {
 		layoutType: "default",
-		isMobile: false,
 		isOnTransition: false,
 	}
 
@@ -58,35 +42,13 @@ export default class Layout extends React.Component {
 	}
 
 	componentDidMount() {
-		this.enquireHandler = enquireScreen(mobile => {
-			const { isMobile } = this.state
-
-			if (isMobile !== mobile) {
-				window.isMobile = mobile
-				this.setState({
-					isMobile: mobile,
-				})
-			}
-
-			if (mobile) {
-				window.app.eventBus.emit("mobile_mode")
-				this.setLayout("mobile")
-			} else {
-				window.app.eventBus.emit("desktop_mode")
-				this.setLayout("default")
-			}
-		})
-
+		this.setLayout("default")
 		window.app.eventBus.on("transitionStart", () => {
 			this.setState({ isOnTransition: true })
 		})
 		window.app.eventBus.on("transitionDone", () => {
 			this.setState({ isOnTransition: false })
 		})
-	}
-
-	componentWillUnmount() {
-		unenquireScreen(this.enquireHandler)
 	}
 
 	render() {
