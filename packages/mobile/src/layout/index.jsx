4b73f8196d76df309c2e94329a0df03a728df64e
srgooglo <srgooglo@ragestudio.net>
2022-01-11 11:08:18 +0100

    update mobile layout

--- packages/mobile/src/layout/index.jsx
+++ packages/mobile/src/layout/index.jsx
@@ -1,16 +1,12 @@
 import React from "react"
 import classnames from "classnames"
-import * as antd from 'antd'
-import { enquireScreen, unenquireScreen } from 'enquire-js'
+import * as antd from "antd"
 
-import Sidebar from './sidebar'
-import Header from './header'
-import Drawer from './drawer'
-import Sidedrawer from './sidedrawer'
-import BottomBar from "./bottombar"
+import Drawer from "./drawer"
+import BottomBar from "./bottomBar"
 
 const LayoutRenders = {
-	mobile: (props) => {
+	default: (props) => {
 		return <antd.Layout className={classnames("app_layout", ["mobile"])} style={{ height: "100%" }}>
 			<antd.Layout className="content_layout">
 				<antd.Layout.Content className="layout_page">
@@ -21,21 +17,6 @@ const LayoutRenders = {
 			</antd.Layout>
 			<BottomBar user={props.user} />
 			<Drawer />
-		</antd.Layout>
-	},
-	default: (props) => {
-		return <antd.Layout className="app_layout" style={{ height: "100%" }}>
-			<Drawer />
-			<Sidebar user={props.user} />
-			<antd.Layout className="content_layout">
-				<Header />
-				<antd.Layout.Content className="layout_page">
-					<div className={classnames("fade-transverse-active", { "fade-transverse-leave": props.isOnTransition })}>
-						{props.children}
-					</div>
-				</antd.Layout.Content>
-			</antd.Layout>
-			<Sidedrawer />
 		</antd.Layout>
 	}
 }
@@ -58,24 +39,7 @@ export default class Layout extends React.Component {
 	}
 
 	componentDidMount() {
-		this.enquireHandler = enquireScreen(mobile => {
-			const { isMobile } = this.state
-
-			if (isMobile !== mobile) {
-				window.isMobile = mobile
-				this.setState({
-					isMobile: mobile,
-				})
-			}
-
-			if (mobile) {
-				window.app.eventBus.emit("mobile_mode")
-				this.setLayout("mobile")
-			} else {
-				window.app.eventBus.emit("desktop_mode")
-				this.setLayout("default")
-			}
-		})
+		this.setLayout("default")
 
 		window.app.eventBus.on("transitionStart", () => {
 			this.setState({ isOnTransition: true })
@@ -83,10 +47,6 @@ export default class Layout extends React.Component {
 		window.app.eventBus.on("transitionDone", () => {
 			this.setState({ isOnTransition: false })
 		})
-	}
-
-	componentWillUnmount() {
-		unenquireScreen(this.enquireHandler)
 	}
 
 	render() {
