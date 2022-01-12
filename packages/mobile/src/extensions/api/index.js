4ce297dd98e4020b629eb89e309a3c6d5b927247
srgooglo <srgooglo@ragestudio.net>
2022-01-12 10:27:17 +0100

    added `WSInterface`

--- packages/mobile/src/extensions/api/index.js
+++ packages/mobile/src/extensions/api/index.js
@@ -1,7 +1,31 @@
-import config from 'config'
+import config from "config"
 import { Bridge } from "linebridge/client"
 import { Session } from "models"
 import io from "socket.io-client"
+
+class WSInterface {
+    constructor(params = {}) {
+        this.params = params
+        this.manager = new io.Manager(this.params.origin)
+        this.sockets = {}
+
+        this.initialize()
+    }
+
+    initialize = () => {
+        this.register("/", "main")
+    }
+
+    register = (socket, as) => {
+        if(typeof socket !== "string") {
+            console.error("socket must be string")
+            return false
+        }
+
+        socket = this.manager.socket(socket)
+        return this.sockets[as ?? socket] = socket
+    }
+}
 
 export default {
     key: "apiBridge",
@@ -10,23 +34,51 @@ export default {
             mutateContext: {
                 async initializeDefaultBridge() {
                     this.apiBridge = await this.createBridge()
-                    this.ws = io(config.ws.address, { transports: ["websocket"] })
+                    this.WSInterface = new WSInterface({
+                        origin: config.ws.address
+                    })
+                    this.WSSockets = this.WSInterface.sockets
 
-                    this.ws.on("connect", (...context) => {
-                        window.app.eventBus.emit("websocket_connected", ...context)
+                    this.WSSockets.main.on("connect", () => {
+                        window.app.eventBus.emit("websocket_connected")
                     })
 
-                    this.ws.on("disconnect", (...context) => {
+                    this.WSSockets.main.on("disconnect", (...context) => {
                         window.app.eventBus.emit("websocket_disconnected", ...context)
                     })
 
-                    this.ws.on("connect_error", (...context) => {
+                    this.WSSockets.main.on("connect_error", (...context) => {
                         window.app.eventBus.emit("websocket_connection_error", ...context)
                     })
 
-                    window.app.ws = this.ws
+                    window.app.ws = this.WSInterface
                     window.app.api = this.apiBridge
                     window.app.request = this.apiBridge.endpoints
+                    window.app.handleWSListener = this.handleWSListener
+                },
+                handleWSListener: (to, fn) => {
+                    if (typeof to === "undefined") {
+                        console.error("handleWSListener: to must be defined")
+                        return false
+                    }
+                    if (typeof fn !== "function") {
+                        console.error("handleWSListener: fn must be function")
+                        return false
+                    }
+                   
+                    let ns = "main"
+                    let event = null
+
+                    if (typeof to === "string") {
+                        event = to
+                    }else if (typeof to === "object") {
+                        ns = to.ns
+                        event = to.event
+                    }
+
+                    return window.app.ws.sockets[ns].on(event, async (...context) => {
+                        return await fn(...context)
+                    })
                 },
                 createBridge: async () => {
                     const getSessionContext = async () => {
