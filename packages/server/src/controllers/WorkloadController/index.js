fbdab9dcfe778ca25a69e8cf93640164fdf8b65e
srgooglo <srgooglo@ragestudio.net>
2022-01-19 13:14:50 +0100

    multiple changes

--- packages/server/src/controllers/WorkloadController/index.js
+++ packages/server/src/controllers/WorkloadController/index.js
@@ -6,27 +6,61 @@ import moment from "moment"
 const format = "DD-MM-YYYY hh:mm"
 
 export default {
-    commit: Schematized({
-        required: ["_id"],
-        select: ["_id"],
+    pushCommit: Schematized({
+        required: ["workloadId", "payloadUUID", "quantity"],
+        select: ["workloadId", "payloadUUID", "quantity"],
     }, async (req, res) => {
+        let workload = await Workload.findById(req.selection.workloadId)
+
+        if (!workload) {
+            return res.status(404).send({
+                error: "Workload not found",
+            })
+        }
+
+        if (typeof workload.commits === "undefined" || !Array.isArray(workload.commits)) {
+            workload.commits = Array()
+        }
+
         let commit = {
-            _id: nanoid(),
+            payloadUUID: req.selection.payloadUUID,
+            quantity: req.selection.quantity,
             user_id: req.user._id,
             timestamp: new Date().getTime(),
         }
 
-        const result = await Workload.findByIdAndUpdate(req.selection._id, {
-            $push: {
-                commits: commit,
-            },
-        }).catch(err => {
+        workload.commits.push(commit)
+
+        await Workload.findByIdAndUpdate(req.selection.workloadId, workload).catch(err => {
             return res.status(500).json({
                 error: err,
             })
         })
 
-        return res.json(result)
+        req.ws.io.emit("workloadCommit", {
+            workloadId: req.selection.workloadId,
+            commit,
+        })
+
+        return res.json(workload.commits)
+    }),
+    getCommits: Schematized({
+        required: ["workloadId"],
+        select: ["workloadId", "payloadUUID"],
+    }, async (req, res) => {
+        let workload = await Workload.findById(req.selection.workloadId)
+
+        if (!workload) {
+            return res.status(404).send({
+                error: "Workload not found",
+            })
+        }
+
+        if (req.selection.payloadUUID) {
+            return res.json(workload.commits.filter(commit => commit.payloadUUID === req.selection.payloadUUID))
+        }
+
+        return res.json(workload.commits)
     }),
     appendOperators: Schematized({
         required: ["_id", "operators"],
@@ -41,10 +75,10 @@ export default {
                         workload.assigned.push(operator)
 
                         // send update with WS
-                        const userSocket = req.getClientSocket(operator)
+                        const userSocket = req.ws.getClientSocket(operator)
 
                         if (userSocket) {
-                            userSocket.socket.emit("workloadAssigned", workload._id)
+                            userSocket.emit("workloadAssigned", workload._id)
                         }
                     }
                 }
@@ -71,10 +105,10 @@ export default {
                     }
 
                     // send update with WS
-                    const userSocket = req.getClientSocket(operator)
+                    const userSocket = req.ws.getClientSocket(operator)
 
                     if (userSocket) {
-                        userSocket.socket.emit("workloadUnassigned", workload._id)
+                        userSocket.emit("workloadUnassigned", workload._id)
                     }
                 }
             } else {
@@ -96,13 +130,13 @@ export default {
         return res.json(workload)
     }),
     set: Schematized({
-        required: ["orders", "region", "name"],
+        required: ["payloads", "region", "name"],
     }, async (req, res) => {
-        const { orders, region, name, assigned, scheduledStart, scheduledFinish, workshift } = req.body
+        const { payloads, region, name, assigned, scheduledStart, scheduledFinish, workshift } = req.body
 
         const obj = {
             created: new Date().getTime(),
-            orders,
+            payloads,
             assigned,
             name,
             scheduledStart,
@@ -111,16 +145,14 @@ export default {
             region,
         }
 
-        // create on each item an UUID property
-        orders.forEach(item => {
+        // create on each payload an UUID property
+        payloads.forEach(item => {
             item.uuid = nanoid()
         })
 
         const result = await Workload.create(obj)
 
-        if (result) {
-            req.io.emit("newWorkload", result)
-        }
+        req.ws.io.emit("newWorkload", result)
 
         return res.json(result)
     }),
@@ -140,7 +172,9 @@ export default {
 
         for await (let _id of queue) {
             const result = await Workload.findByIdAndDelete({ _id })
+
             if (result != null) {
+                req.ws.io.emit("deletedWorkload", _id)
                 deleted.push(_id)
             }
         }
@@ -148,36 +182,58 @@ export default {
         return res.json({ deleted })
     }),
     get: Schematized({
-        select: ["region", "_id", "name"],
-    }, async (req, res, next) => {
-        let workload = await Workload.findOne(req.selection)
-
-        // parse expiration status
-        if (typeof workload.expired !== "undefined") {
-            // check if is expired
-            const endDate = moment(workload.scheduledFinish, format)
-            const now = moment().format(format)
-
-            if (endDate.isBefore(moment(now, format))) {
-                workload.expired = true
-            }
-        }
-
-        return res.json(workload)
-    }),
-    getAll: Schematized({
-        select: ["region", "_id", "name"],
+        select: ["_id", "region", "name"],
     }, async (req, res) => {
-        let workloads = []
+        let workloads = null
 
         if (req.selection.region === "all") {
             delete req.selection.region
-            workloads = await Workload.find(req.selection)
+        }
+
+        if (req.selection._id) {
+            const data = await Workload.findById(req.selection._id)
+
+            if (!data) {
+                return res.status(404).send({
+                    error: "Workload not found",
+                })
+            }
+
+            workloads = [data]
         } else {
             workloads = await Workload.find(req.selection)
         }
 
-        return res.json(workloads)
+        for await (const workload of workloads) {
+            // parse expiration status
+            if (typeof workload.expired !== "undefined") {
+                // check if is expired
+                const endDate = moment(workload.scheduledFinish, format)
+                const now = moment().format(format)
+
+                if (endDate.isBefore(moment(now, format))) {
+                    workload.expired = true
+                }
+            }
+        }
+
+        return res.json(req.selection._id ? workloads[0] : workloads)
+    }),
+    getWorkloadWithPayloadUUID: Schematized({
+        required: ["uuid"],
+        select: ["uuid"],
+    }, async (req, res) => {
+        const workload = await Workload.findOne({
+            "payloads.uuid": req.selection.uuid,
+        })
+
+        if (!workload) {
+            return res.status(404).send({
+                error: "Workload not found",
+            })
+        }
+
+        return res.json(workload)
     }),
     getWorkloadAssignedToUserID: Schematized({
         select: ["_id"],
@@ -192,9 +248,9 @@ export default {
         let workloads = await Workload.find()
 
         workloads.forEach(async (workload) => {
-            if (workload.orders.length > 0) {
+            if (workload.payloads.length > 0) {
 
-                workload.orders.forEach((item) => {
+                workload.payloads.forEach((item) => {
                     if (typeof item.uuid === "undefined") {
                         item.uuid = nanoid()
                     }
