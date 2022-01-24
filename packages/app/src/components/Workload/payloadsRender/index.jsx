6867e1519e7827b51784c838a0379b5df3999c46
srgooglo <srgooglo@ragestudio.net>
2022-01-24 14:36:11 +0100

    improve style

--- packages/app/src/components/Workload/payloadsRender/index.jsx
+++ packages/app/src/components/Workload/payloadsRender/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Icons } from "components/Icons"
 import * as antd from "antd"
 import classnames from "classnames"
 
@@ -20,24 +21,30 @@ export default (props) => {
 	return <antd.List
 		dataSource={props.payloads}
 		renderItem={(item) => {
-			return <antd.List.Item
+			return <div
 				onClick={() => { onClickItem(item) }}
 				className={classnames("workload_payloadItem", { ["reached"]: item.quantityReached })}
 			>
-				<antd.List.Item.Meta
-					avatar={<div className="workload_payloadItem quantity">x{item.properties?.quantity ?? item.quantity ?? "0"}</div>}
-					title={item.name}
-					description={item.properties?.description}
-				/>
-				<div>
-					{item.selectedVariants && <div>
-						<div>{item.selectedVariants.length} Variants</div>
+				<div className="data">
+					<div className="header">
+						<div className="title">
+							<div className="quantity">x{props.preview ? (item.properties?.quantity) : (item.debtQuantity ?? item.properties?.quantity ?? "0")}</div>
+							<h2>{item.name}</h2>
+						</div>
+						<div className="description">{item.properties?.description ?? "No description"}</div>
+					</div>
+
+					{item.properties?.variants && <div className="variants">
+						<Icons.Tool /> {item.properties.variants.map((variant, index) => <antd.Tag key={index}>{variant}</antd.Tag>)}
 					</div>}
 				</div>
-				{props.onDeleteItem && <antd.Button onClick={() => onClickDelete(item)} type="link">
-					Delete
-				</antd.Button>}
-			</antd.List.Item>
+
+				<div className="actions">
+					{props.onDeleteItem && <antd.Button onClick={() => onClickDelete(item)} type="link">
+						Delete
+					</antd.Button>}
+				</div>
+			</div>
 		}}
 	/>
 }
