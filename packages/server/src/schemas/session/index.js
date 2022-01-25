e972f55b13579700537b82b7d9e2b047ef605a4e
srgooglo <srgooglo@ragestudio.net>
2022-01-25 13:10:46 +0100

    update schemas

--- packages/server/src/schemas/session/index.js
+++ packages/server/src/schemas/session/index.js
@@ -1,9 +1,7 @@
 export default {
-    allowRegenerate: { type: Boolean, default: false },
-    uuid: { type: String, required: true },
+    session_uuid: { type: String, required: true },
     token: { type: String, required: true },
     user_id: { type: String, required: true },
     date: { type: Number, default: 0 },
     location: { type: String, default: "Unknown" },
-    geo: { type: String, default: "Unknown" },
 }
--- packages/server/src/schemas/user/index.js
+++ packages/server/src/schemas/user/index.js
@@ -1,4 +1,5 @@
 export default {
+    refreshToken: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true, select: false },
     fullName: String,
