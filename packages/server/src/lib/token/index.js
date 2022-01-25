85e5a047206e82484cdf06906f837c1e0b141ac5
srgooglo <srgooglo@ragestudio.net>
2022-01-25 13:11:03 +0100

    refactor lib

--- packages/server/src/lib/token/index.js
+++ packages/server/src/lib/token/index.js
@@ -1,29 +1,50 @@
-import jwt from 'jsonwebtoken'
-import { nanoid } from 'nanoid'
-import { Session } from '../../models'
+import jwt from "jsonwebtoken"
+import { nanoid } from "nanoid"
+import { Session, User } from "../../models"
 
-export function signNew(payload, options) {
-    const data = {
-        uuid: nanoid(),
-        allowRegenerate: false,
-        ...payload
+export async function createNewAuthToken(user, options = {}) {
+    const payload = {
+        user_id: user._id,
+        username: user.username,
+        email: user.email,
+        refreshToken: nanoid(),
     }
 
-    const token = jwt.sign(data, options.secretOrKey, {
+    await User.findByIdAndUpdate(user._id, { refreshToken: payload.refreshToken })
+
+    return await signNew(payload, options)
+}
+
+export async function signNew(payload, options = {}) {
+    let session_uuid = null
+
+    if (options.updateSession) {
+        session_uuid = options.updateSession
+    } else {
+        session_uuid = nanoid()
+    }
+
+    const token = jwt.sign({
+        ...payload,
+        session_uuid,
+    }, options.secretOrKey, {
         expiresIn: options.expiresIn ?? "1h",
         algorithm: options.algorithm ?? "HS256"
     })
 
-    let newSession = new Session({
-        uuid: data.uuid,
-        user_id: data.user_id,
-        allowRegenerate: data.allowRegenerate,
-        token: token,
-        date: new Date().getTime(),
-        location: options.sessionLocationSign
-    })
+    if (options.updateSession) {
+        await Session.findByIdAndUpdate(options.updateSession, { token })
+    } else {
+        let newSession = new Session({
+            session_uuid,
+            token: token,
+            user_id: payload.user_id,
+            date: new Date().getTime(),
+            location: global.signLocation ?? "rs-auth",
+        })
 
-    newSession.save()
+        newSession.save()
+    }
 
     return token
 }
