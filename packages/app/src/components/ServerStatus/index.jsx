6ef13afa6584f7f73a260e4e331eefb374e34212
srgooglo <srgooglo@ragestudio.net>
2022-02-02 10:01:38 +0100

    default state with `mainSocketConnected`

--- packages/app/src/components/ServerStatus/index.jsx
+++ packages/app/src/components/ServerStatus/index.jsx
@@ -3,7 +3,7 @@ import * as antd from "antd"
 import { Icons } from "components/Icons"
 
 export default () => {
-    const [connected, setConnected] = React.useState(window.app.ws.connected ?? false)
+    const [connected, setConnected] = React.useState(window.app.ws.mainSocketConnected ?? false)
 
     window.app.eventBus.on("websocket_connected", (status) => {
         setConnected(true)
