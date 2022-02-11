b9494b23a15e4321dcf510ab52c51029f1c0233d
srgooglo <srgooglo@ragestudio.net>
2022-02-11 14:54:53 +0100

    added `fileUpload`

--- packages/server/src/middlewares/index.js
+++ packages/server/src/middlewares/index.js
@@ -1,4 +1,8 @@
-export { default as withAuthentication } from './withAuthentication'
-export { default as errorHandler } from './errorHandler'
-export { default as hasPermissions } from './hasPermissions'
-export { default as roles } from './roles'
+const fileUpload = require("@nanoexpress/middleware-file-upload/cjs")()
+
+export { default as withAuthentication } from "./withAuthentication"
+export { default as errorHandler } from "./errorHandler"
+export { default as hasPermissions } from "./hasPermissions"
+export { default as roles } from "./roles"
+
+export { fileUpload as fileUpload }
