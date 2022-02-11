eae0e1d727b25fda9eda86de8b9af7a7b7b00e7f
srgooglo <srgooglo@ragestudio.net>
2022-02-11 13:07:01 +0100

    added `getBase64` util

--- packages/app/src/utils/index.js
+++ packages/app/src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as useLongPress } from "./useLongPress"
 export { default as findChildById } from "./findChildById"
 export { default as cursorPosition } from "./cursorPosition"
+export { default as getBase64 } from "./getBase64"
