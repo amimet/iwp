aca47cce15ed77e06718f9497c31f5ad382ff5ad
srgooglo <srgooglo@ragestudio.net>
2022-02-15 13:55:01 +0100

    implement `Notifications` extensions

--- packages/app/src/extensions/index.js
+++ packages/app/src/extensions/index.js
@@ -3,6 +3,7 @@ export * as Splash from "./splash"
 export * as Sound from "./sound"
 export * as Theme from "./theme"
 export * as i18n from "./i18n"
+export * as Notifications from "./notifications"
 
 export { default as SettingsController } from "./settings"
 export { default as API } from "./api"
