1c68b5e50ad452afd6ff76bee19225f58d70cf3e
srgooglo <srgooglo@ragestudio.net>
2022-02-16 10:33:57 +0100

    added `Haptic` util

---
+++ packages/app/src/utils/haptics/index.js
@@ -0,0 +1,25 @@
+import { Haptics, ImpactStyle } from "@capacitor/haptics"
+
+export default {
+    selectionStart: async () => {
+        const enabled = window.app.settings.get("haptic_feedback")
+
+        if (enabled) {
+            await Haptics.selectionStart()
+        }
+    },
+    selectionChanged: async () => {
+        const enabled = window.app.settings.get("haptic_feedback")
+
+        if (enabled) {
+            await Haptics.selectionChanged()
+        }
+    },
+    selectionEnd: async () => {
+        const enabled = window.app.settings.get("haptic_feedback")
+
+        if (enabled) {
+            await Haptics.selectionEnd()
+        }
+    },
+}
