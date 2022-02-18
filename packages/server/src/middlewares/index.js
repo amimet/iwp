16110c84b6d57cef10a3a996bd4034c5c8160b77
srgooglo <srgooglo@ragestudio.net>
2022-02-18 14:54:50 +0100

    export `privileged`

--- packages/server/src/middlewares/index.js
+++ packages/server/src/middlewares/index.js
@@ -4,5 +4,6 @@ export { default as withAuthentication } from "./withAuthentication"
 export { default as errorHandler } from "./errorHandler"
 export { default as hasPermissions } from "./hasPermissions"
 export { default as roles } from "./roles"
+export { default as privileged } from "./privileged"
 
 export { fileUpload as fileUpload }
