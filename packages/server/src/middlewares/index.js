fc749adc15e57caddcbf36dbda149f720f427206
srgooglo <srgooglo@ragestudio.net>
2022-02-22 12:45:37 +0100

    update exports

--- packages/server/src/middlewares/index.js
+++ packages/server/src/middlewares/index.js
@@ -4,6 +4,7 @@ export { default as withAuthentication } from "./withAuthentication"
 export { default as errorHandler } from "./errorHandler"
 export { default as hasPermissions } from "./hasPermissions"
 export { default as roles } from "./roles"
-export { default as privileged } from "./privileged"
+export { default as onlyAdmin } from "./onlyAdmin"
+export { default as permissions } from "./permissions"
 
 export { fileUpload as fileUpload }
